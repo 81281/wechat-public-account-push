8811e57cdc6a7ca1bdbbf82b4a8cb1a782ef147c
81281 <114729055+81281@users.noreply.github.com>
2022-09-30 16:51:25 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -179,7 +179,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: ' A-IvD-IPq-t6KqDN4ZC8LiSShihyMcnyimqVX796On8 '	,
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [Aug9_W25]
